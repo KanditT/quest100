582e9fac041b853b37c82ba91222c5bb8905e737
KanditT <kandit.t@kkumail.com>
2025-08-16 14:34:10 +0700

    test: rspec model, route, request

--- app/models/quest.rb
+++ app/models/quest.rb
@@ -2,4 +2,12 @@
 
 class Quest < ApplicationRecord
   validates :name, presence: true
+
+  after_initialize :set_default_status, if: :new_record?
+
+  private
+
+  def set_default_status
+    self.status ||= false
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :quests
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -11,7 +10,7 @@ Rails.application.routes.draw do
   # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
-  resources :quests do
+  resources :quests, only: %i[index create destroy] do
     member do
       patch :toggle_status
     end
