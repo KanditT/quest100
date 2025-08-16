3f4664a5f75f59063964222b577e8c2dc3e67627
KanditT <kandit.t@kkumail.com>
2025-08-16 13:15:19 +0700

    feat: index page with create quest

---
+++ app/javascript/controllers/quest_form_controller.js
@@ -0,0 +1,19 @@
+// app/javascript/controllers/quest_form_controller.js
+import { Controller } from "@hotwired/stimulus";
+
+export default class extends Controller {
+    static targets = ["form"];
+
+    connect() {
+        this.element.addEventListener(
+            "turbo:submit-end",
+            this.handleSubmitEnd.bind(this)
+        );
+    }
+
+    handleSubmitEnd(event) {
+        if (event.detail.success) {
+            this.formTarget.reset();
+        }
+    }
+}
