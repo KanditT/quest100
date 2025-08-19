1aebd34893e17dab57a8385dfcc3ea535c17b342
KanditT <kandit.t@kkumail.com>
2025-08-19 18:26:43 +0700

    casc

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,6 +4,8 @@ ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rails/test_help'
 
+# Previous content of test helper now starts here
+
 module ActiveSupport
   class TestCase
     # Run tests in parallel with specified workers
